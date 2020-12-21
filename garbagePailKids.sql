9125e7ff81231f2ddf6b6e144cd98e72549241ce
Carrye Gaulin <carryegaulin033178@gmail.com>
2020-12-21 14:32:16 -0500

    creating new sql file with table breakdown

---
+++ garbagePailKids.sql
@@ -0,0 +1,51 @@
+DROP DATABASE GPK;
+CREATE DATABASE GPK;
+
+CREATE USER 'gpk'@'localhost' IDENTIFIED BY 'gpk';
+
+GRANT ALL ON novels.* TO 'gpk'@'localhost';
+
+USE GPK;
+
+CREATE TABLE cardListA (
+  id INT auto_increment,
+  name VARCHAR(255) NOT NULL,
+  list ENUM('A', 'B'),
+  createdAt DATETIME DEFAULT NOW(),
+  updatedAt DATETIME DEFAULT NOW() ON UPDATE NOW(),
+  deletedAt DATETIME,
+  PRIMARY KEY(id)
+);
+
+CREATE TABLE cardListB (
+  id INT auto_increment,
+  name VARCHAR(255) NOT NULL,
+  list ENUM('A', 'B'),
+  createdAt DATETIME DEFAULT NOW(),
+  updatedAt DATETIME DEFAULT NOW() ON UPDATE NOW(),
+  deletedAt DATETIME,
+  PRIMARY KEY(id)
+);
+
+CREATE TABLE seriesData (
+  id INT auto_increment,
+  seriesNo INT NOT NULL,
+  releaseDate INT NOT NULL,
+  cardNo INT NOT NULL,
+  createdAt DATETIME DEFAULT NOW(),
+  updatedAt DATETIME DEFAULT NOW() ON UPDATE NOW(),
+  deletedAt DATETIME,
+  PRIMARY KEY(id)
+);
+
+CREATE TABLE cardListLinking (
+  cardListAId INT,
+  cardListBId INT,
+  seriesDataId INT
+  createdAt DATETIME DEFAULT NOW(),
+  updatedAt DATETIME DEFAULT NOW() ON UPDATE NOW(),
+  deletedAt DATETIME,
+  FOREIGN KEY (cardListAId) REFERENCES cardListAId(id),
+  FOREIGN KEY (cardListBId) REFERENCES cardListB(id),
+  FOREIGN KEY (seriesDataId) REFERENCES seriesData(id)
+);
