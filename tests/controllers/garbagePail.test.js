7486a87bb353625a8d796ca24be9ed9ebb3f1497
Carrye Gaulin <carryegaulin033178@gmail.com>
2020-12-21 13:42:57 -0500

    mock folder and file add

--- tests/controllers/garbagePail.test.js
+++ tests/controllers/garbagePail.test.js
@@ -2,6 +2,8 @@ const chai = require('chai')
 const sinon = require('sinon')
 const sinonChai = require('sinon-chai')
 const { describe, it } = require('mocha')
+const { kidsList } = require('../mocks/heroes')
+
 const { getAllCards, getKidByName, saveNewKid } = require('../../controllers/garbagePail')
 
 chai.use(sinonChai)
---
+++ tests/mocks/mocks.js
@@ -0,0 +1,24 @@
+const kidsList = [{
+  name: 'April Showers',
+  list: 'B',
+  seriesNo: '1',
+  releaseDate: '1985',
+  cardNo: '3'
+},
+{
+  name: 'Cranky Frankie',
+  list: 'A',
+  seriesNo: '1',
+  releaseDate: '1985',
+  cardNo: '18'
+}]
+
+const singleKid = {
+  name: 'Scary Carrie',
+  list: 'B',
+  seriesNo: '1',
+  releaseDate: '1985',
+  cardNo: '25'
+}
+
+module.exports = { heroesList, singleHero }
