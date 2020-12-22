7b009609e478f04d27c964494042b05310f9b940
Carrye Gaulin <carryegaulin033178@gmail.com>
2020-12-21 20:16:17 -0500

    models index.js update

--- models/index.js
+++ models/index.js
@@ -1,10 +1,40 @@
 const Sequelize = require('sequelize')
-const kidsModel = require('./GarbagePail')
+const allConfigs = require('../config/sequelize')
+const CardListAModel = require('./cardListA')
+const CardListBModel = require('./cardListB')
+const SeriesDataModel = require('./seriesData')
+const CardASeriesLinkingModel = require('./cardASeriesLinking')
+const CardBSeriesLinkingModel = require('./cardBSeriesLinking')
+const cardListA = require('./cardListA')
+const seriesData = require('./seriesData')
+const cardListB = require('./cardListB')
+const cardASeriesLinking = require('./cardASeriesLinking')
+const cardBSeriesLinking = require('./cardBSeriesLinking')
 
-const connection = new Sequelize('GarbagePail', 'GarbagePail', 'Dalla$$', {
-  host: 'localhost', dialect: 'mysql'
+const environment = process.env.NODE_ENV || 'development'
+const config = allConfigs[environment]
+
+const connection = new Sequelize(config.database, config.username, config.password, {
+  host: config.host, dialect: config.dialect
 })
 
-const kids = kidsModel(connection, Sequelize)
+const CardListA = CardListAModel(connection, Sequelize)
+const CardListB = CardListBModel(connection, Sequelize)
+const SeriesData = SeriesDataModel(connection, Sequelize)
+const CardASeriesLinking = CardASeriesLinkingModel(connection, Sequelize, cardListA, seriesData)
+const CardBSeriesLinking = CardBSeriesLinkingModel(connection, Sequelize, cardListB, seriesData)
 
-module.exports = { kids }
+cardASeriesLinking.belongTo(cardListA)
+cardASeriesLinking.hasMany(seriesData)
+cardBSeriesLinking.belongTo(cardListB)
+CardBSeriesLinking.hasMany(seriesData)
+
+
+module.exports = {
+  CardListA,
+  CardListB,
+  SeriesData,
+  CardASeriesLinking,
+  CardBSeriesLinking,
+  Op: Sequelize.Op,
+}
